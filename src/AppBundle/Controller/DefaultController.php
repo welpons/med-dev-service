a8b94ffb81728d0d6703af610ec67b1fe52e2512
Felix Pons <welpons@gmail.com>
2017-02-27 14:22:31 +0100

    Refactoring AppBundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,8 +14,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $userProgramApi = $this->get('userprogram');
-
-        $measurementTasks = $userProgramApi->getMeasurementTasksByPeriod($measurements);        
+        return new Response('med-dev-service - DDD Sample Application');
     }
 }
