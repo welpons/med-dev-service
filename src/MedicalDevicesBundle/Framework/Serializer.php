bc674cfbe5f4d63dfceccd4a7283c38b4bb03155
Felix Pons <welpons@gmail.com>
2017-02-27 14:25:26 +0100

    Migrating Serializer service from AppBundle to MedicalDeviceBundle

---
+++ src/MedicalDevicesBundle/Framework/Serializer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MedicalDevicesBundle\Framework;
+
+use JMS\Serializer\SerializerBuilder;
+use MedicalDevices\Infrastructure\Service\External\SerializerServiceInterface;
+use MedicalDevices\Configuration\ConfigurationInterface;
+
+/**
+ * Class used as service. It allows to denormalize: convert arrays into objects
+ * This service is based on JMS Serializer
+ * 
+ */
+class Serializer implements SerializerServiceInterface
+{
+    private $metaDataDir = null;    
+    private $serializer;
+    
+    public function __construct(ConfigurationInterface $configurations)
+    {
+        $this->metaDataDir = $configurations->getParameter('infrastructure.jms_serializer_mapping_dir');
+    }
+
+    public function setMetadataDir(string $metaDataDir)
+    {
+        if (!is_dir($metaDataDir)) {
+            throw new \InvalidArgumentException(sprintf('The directory "%s" does not exist.', $metaDataDir));
+        }        
+        
+        $this->metaDataDir = $metaDataDir;           
+        
+        return $this;
+    }        
+    
+    public function deserialize($data, $type, $format = 'xml')
+    {
+        $this->serializer = SerializerBuilder::create()->addMetadataDir($this->metaDataDir)->build();          
+        
+        return $this->serializer->deserialize($data, $type, $format);
+    }
+
+}
