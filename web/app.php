f2f3417d66c5d447182ca9be9f7fa8bf832d49b3
Felix Pons <welpons@gmail.com>
2017-05-30 14:53:30 +0200

    Upgrading to symfony 3.3.0

--- web/app.php
+++ web/app.php
@@ -13,6 +13,7 @@ $kernel->loadClassCache();
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+Request::setTrustedProxies(['192.0.0.1', '10.0.0.0/8'], Request::HEADER_X_FORWARDED_ALL);
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
